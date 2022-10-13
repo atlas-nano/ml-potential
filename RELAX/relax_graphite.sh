0a5715fbdbfb083ca2fdad2f1bc3272e99cdcbbe
Robert Scanlon Ramji <rramji@ucsd.edu>
2022-10-13 09:34:47 -0700

    lammps testing

--- RELAX/relax_graphite.sh
+++ RELAX/relax_graphite.sh
@@ -5,4 +5,4 @@ export OMP_NUM_THREADS=1
 in="../CFG/test_structures/graphite_high_eng.cfg"
 out="graphite_relaxed.cfg"
 
-mlp relax relax.ini --cfg-filename=$in --save-relaxed=$out --force-tolerance=1e-4 --stress-tolerance=1e-3
+mpirun -n 1 mlp relax relax.ini --cfg-filename=$in --save-relaxed=$out --force-tolerance=1e-5 --stress-tolerance=1e-4
---
+++ RELAX/relax_lic12.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=1
+
+in="../CFG/test_structures/lic12_high_eng.cfg"
+out="lic12_relaxed.cfg"
+
+mpirun -n 1 mlp relax relax.ini --cfg-filename=$in --save-relaxed=$out --force-tolerance=1e-4 --stress-tolerance=1e-3
